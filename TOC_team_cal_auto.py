74dcd171cc757f35adef6f923cf2ee908a8c818a
Kyle Lockwood <58640375+kylelockwood@users.noreply.github.com>
2019-12-30 20:53:01 -0800

    Create TOC_team_cal_auto.py

---
+++ TOC_team_cal_auto.py
@@ -0,0 +1,9 @@
+#! python3
+# Uses data from team schedule worksheet and creates calendar events on the TOC site
+
+# TODO Imports
+# TODO get relevant excel data from sheet
+# TODO open site in browser, full screen
+# TODO click through to the calendar
+# TODO create new events
+# TODO fill out the event data
