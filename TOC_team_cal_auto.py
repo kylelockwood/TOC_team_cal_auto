21f88a5f75c97baff7b9a0d5eb4cc41ba5234e14
Kyle Lockwood <58640375+kylelockwood@users.noreply.github.com>
2019-12-31 12:52:05 -0800

    Update TOC_team_cal_auto.py

--- TOC_team_cal_auto.py
+++ TOC_team_cal_auto.py
@@ -5,23 +5,6 @@ import pyautogui, openpyxl
 from datetime import timedelta
 from ics import Calendar, Event
 
-"""
-class Unbuffered(object):
-   def __init__(self, stream):
-       self.stream = stream
-   def write(self, data):
-       self.stream.write(data)
-       self.stream.flush()
-   def writelines(self, datas):
-       self.stream.writelines(datas)
-       self.stream.flush()
-   def __getattr__(self, attr):
-       return getattr(self.stream, attr)
-
-import sys
-sys.stdout = Unbuffered(sys.stdout)
-"""
-
 def get_xl_data(sheet):
     print(f'Parsing calendar data... ', flush=True, end='')
     # Return the dates and schedule data
